33b82d5c54c56f05854aa6eba19daac05ae4f06e
kauarba <kaushikbaruaa@gmail.com>
2016-01-25 13:08:13 +0530

    Front end source code

---
+++ static/includes/js/script.js
@@ -0,0 +1,25 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+	
+	
+
+	$('#submitButton').click(function(e) {
+		document.getElementById("emailId").value = "";
+	});
+
+	$('#subButton').click(function(e) {
+		document.getElementById("emailIdBottom").value = "";
+	});
+	
+	
+});
